2cb5e22493bd8c0be833bb13edee733014ed3dfa
Dani-Masus <1204384@spsd.sk.ca>
2025-09-10 14:09:38 -0600

    almost done

--- 01 First program/sketch.js
+++ 01 First program/sketch.js
@@ -1,19 +1,35 @@
-// Project Title
-// Kingston
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// coordinate system/User events
+// Mr. Scott
+// sept 10, 2025
+//first look in interactive programs with p5.js
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  print(windowWidth, windowHeight, width, height)
+  //windowwidth; current browser width
+  //width;            canvas width
 }
 
 function draw() { //runs over and over
-                  // Targeting 60 frames per sec
-  background(220);
-  fill(255,mouseX,0);
-  circle(mouseX,mouseY, 30);
+  // Targeting 60 frames per sec
+
+  //Strive to keep draw() clean and tidy
+
+  background(220);  //draws a big solid retangle
+  //  0 - black   255 - white
+
+  DrawTwoCircles();
+
 }
 
+function DrawTwoCircles() {  //{alt}{shift}{f} = autoformate
+  //draw two circles at a fixed location
+  //and one at mouse location
+  fill(0, 225, 0);
+  //  r,   g,   b
+  circle(mouseX, mouseY, 30);
+
+  circle(width/2, height/2, 100);
+
+}
