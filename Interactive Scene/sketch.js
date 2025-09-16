e48c5b4a89d7bbff36fbd035b1ab49a14b895162
Dani-Masus <1204384@spsd.sk.ca>
2025-09-16 14:09:18 -0600

    background work
    not going so well... bout it can only get better!

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -1,9 +1,6 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Interactive Scene. Gurama
+// Kingston
+// sept. 16, 2025
 
 
 function setup() {
@@ -12,4 +9,26 @@ function setup() {
 
 function draw() {
   background(220);
+
+
+  fill("#F0F8FF");
+
+  circle(50,50,50);
+  circle(80,50,50);
+  circle(20,50,50);
+  circle(30,30,50);
+  circle(50,78,50);
+  circle(90,30,50);
+  
+
+  triangle(200, 405, 318, 30, 506, 405);
+  triangle(300, 705, 508, 200, 806, 705);
+  triangle(400, 850, 680, 300, 960,850);
+  triangle(100, 405, 208, 50, 406, 505);
+
+  ellipse(700,600,600,500);
+  ellipse(200,600,700,500);
+  ellipse(400,750,1500,500);
+  
 }
+
