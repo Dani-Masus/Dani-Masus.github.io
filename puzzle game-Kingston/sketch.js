5f00e3edfb56ddff14c693b05e2de3697a71fae4
Dani-Masus <1204384@spsd.sk.ca>
2025-11-05 14:10:00 -0600

    puzzle power

---
+++ puzzle game-Kingston/sketch.js
@@ -0,0 +1,117 @@
+// 2D Array Basics
+// Mr. Scott
+// Nov 3, 2025
+
+// 0 (black)     255 (white)
+// grid is 5 x 4 in dimension
+
+let grid = [
+  [0, 0, 0, 255, 0],
+  [255, 0, 255, 0, 255],
+  [255, 255, 0, 255, 255],
+  [0, 255, 0, 0, 0]
+];
+
+let rows = grid.length;
+let cols = grid[0].length;
+
+let squareSize = 170;
+
+function setup() {
+  createCanvas(cols * squareSize, rows * squareSize);
+  
+}
+
+
+
+
+function draw() {
+  background(220);
+  renderGrid();
+  print(getCurrentX(), getCurrentY());
+  
+}
+
+
+function winner(){
+  if (grid = fill(0)){
+    print("Winner!!!");
+  }
+}
+
+
+
+
+
+
+
+  function mousePressed() {
+    //flip current tile
+    //upgrade: only do this if the mouse is on Canvas
+
+    let x = getCurrentX();
+    let y = getCurrentY();
+
+    //ALWAYS: flip the "focused" tile
+    flip(x, y);
+
+    //IF THEY EXIST:
+    //flip our NSEW neighbours (cross pattern)
+
+    if (keyIsDown(SHIFT) === false) {
+
+      if (x + 1 < cols) flip(x + 1, y);
+      if (y - 1 >= 0) flip(x, y - 1);
+      if (x - 1 < cols) flip(x - 1, y);
+      if (y + 1 >= 0) flip(x, y + 1);
+    }
+  }
+
+
+
+
+
+  function getCurrentX() {
+    //determine current col of mouse position
+    let constrainedX = constrain(mouseX, 0, width - 1);
+    return floor(constrainedX / squareSize);
+  }
+
+
+
+
+  function getCurrentY() {
+    //determine current row of mouse position
+    let constrainedY = constrain(mouseY, 0, height - 1);
+    return floor(constrainedY / squareSize);
+  }
+
+
+
+
+
+
+  function flip(x, y) {
+    //takes a tile @ x,y and inverts its value
+    if (grid[y][x] === 0) grid[y][x] = 255;
+    else grid[y][x] = 0;
+  }
+
+
+
+
+
+  function renderGrid() {
+    // interpret the information in the 2D array, and draw
+    // a grid of square on the screen to reflect it.
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < cols; x++) {
+        let fillColor = grid[y][x];
+        fill(fillColor);
+        square(x * squareSize, y * squareSize, squareSize);
+      }
+    }
+  }
+
+
+
