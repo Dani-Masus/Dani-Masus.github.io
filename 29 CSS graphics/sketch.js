eaa14678a3ea41f7bc61793b6f9cf588c7def579
Dani-Masus <1204384@spsd.sk.ca>
2025-12-05 14:05:34 -0600

    understanding Godot + 3d action in VS.code

---
+++ 29 CSS graphics/sketch.js
@@ -0,0 +1,40 @@
+// Project Title
+// Your Name
+// Date
+
+
+
+function setup() {
+  createCanvas(800, 600, WEBGL);
+}
+
+function draw() {
+  background(220);
+
+  angle = map(mouseX, 0, width, -120, 120);
+  
+  orbitControl();
+  lights();
+
+  fill(100,255,100);
+  for (let i = 0; i < 360; i+=45){
+    push();
+    rotateY(radians(i));
+    drawBox(100);
+    pop();
+}
+    
+  }
+  
+
+let angle = 10;
+
+function drawBox(size){
+  if (size > 3){
+    rotateZ(radians(angle));
+    translate(size * 1.5, 0);
+    box(size);
+
+    drawBox(size * 0.8);
+  }
+}
