c096793b47f85c6db5b3b6177e79f0aaf856c881
Dani-Masus <1204384@spsd.sk.ca>
2025-12-09 14:11:00 -0600

    movement and camera placement

--- final project git/final-project---git/character/character_body_2d.gd
+++ final project git/final-project---git/character/character_body_2d.gd
@@ -1,18 +1,20 @@
 extends CharacterBody2D
 
-@export var move_speed : float = 100
 
-func _physics_process(_delta);
+@export var move_speed : float = 200
 
-#Input diretion
-var input_direction = Vector2(
-	input.get_action_strength("right") - input.get_action_strength("left"),
-	input.Get_action_strength("down") - input.get_action_strength("left")
-);
+func _physics_process(_delta):
 
+	
 
+	#Input diretion
+	var input_direction = Vector2(
+		Input.get_action_strength("left") - Input.get_action_strength("right"),
+		Input.get_action_strength("up") - Input.get_action_strength("down")
+	);
 
-#update velocity
-#velocity = input_direction * move_speed
-#Move and slide function uses veloicty of characters to move
-move_and_slide()
+		#update velocity
+	velocity = input_direction * move_speed
+
+		#Move and slide function uses veloicty of characters to move
+	move_and_slide()
