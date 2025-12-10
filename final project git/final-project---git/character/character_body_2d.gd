5c49c32ba8138fef881acbe95c801fbe74e1e500
Dani-Masus <1204384@spsd.sk.ca>
2025-12-10 14:03:08 -0600

    movement done + Walk animation start

--- final project git/final-project---git/character/character_body_2d.gd
+++ final project git/final-project---git/character/character_body_2d.gd
@@ -3,14 +3,21 @@ extends CharacterBody2D
 
 @export var move_speed : float = 200
 
+@export var starting_direction : Vector2 = Vector2(0, 1)
+# parameters/idle/blend_position
+@onready var animation_tree = $AnimationTree
+
+func _ready():
+	animation_tree.set("parameters/idle/blend_position", starting_direction)
+
 func _physics_process(_delta):
 
 	
 
 	#Input diretion
 	var input_direction = Vector2(
-		Input.get_action_strength("left") - Input.get_action_strength("right"),
-		Input.get_action_strength("up") - Input.get_action_strength("down")
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
 	);
 
 		#update velocity
