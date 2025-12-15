ac23556c2dafe6367ae41252e8069902fbfd37af
Dani-Masus <1204384@spsd.sk.ca>
2025-12-15 14:07:58 -0600

    making the walking animation somewhat working

--- final project git/final-project---git/character/character_body_2d.gd
+++ final project git/final-project---git/character/character_body_2d.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 @export var starting_direction : Vector2 = Vector2(0, 1)
 # parameters/idle/blend_position
 @onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
 
 func _ready():
 	#animation_tree.set("parameters/idle/blend_position", starting_direction)
@@ -29,9 +30,17 @@ func _physics_process(_delta):
 
 		#Move and slide function uses veloicty of characters to move
 	move_and_slide()
+	
+	pick_new_state()
 
 func update_animation_parameters(move_input : Vector2):
 	#change animation parameters if there is no move input
 	if(move_input != Vector2.ZERO):
-		animation_tree.set("parameters/walk/blend_position", move_input)
+		animation_tree.set("parameters/Walk_Down/blend_position", move_input)
 		animation_tree.set("parameters/idle/blend_position", move_input)
+
+func pick_new_state():
+	if (velocity != Vector2.ZERO):
+		state_machine.travel("Walk_Down")
+	else:
+		state_machine.travel("idle")
